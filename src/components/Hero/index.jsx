101f46d17ced568b7591e0cf889c281228c08700
pedrosantb <pedrosantb@gmail.com>
2025-03-27 12:19:15 +0100

    Feat: Hero no svg

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+'use client';
 
-function Hero() {
+import React from "react";
+
+const Hero = () => {
   return (
-    <>
-        <h1>Hero</h1>
-    </>
-  )
-}
+    <div className="w-full flex flex-col items-center justify-center text-white p-6 relative gap-2">
+      <h1 className="text-6xl md:text-6xl font-extrabold tracking-wide">
+        Welcome to <span className="text-red-500">ShameMe</span>!
+      </h1>
+      <p className="text-lg md:text-3xl mt-2 text-gray-300 text-center">
+        Your personal AI roastmaster. Get shamed into achieving your goals! 😈
+      </p>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
